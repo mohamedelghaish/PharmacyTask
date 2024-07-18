395176153a89d053ca586e34048994f53001425d
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-07-19 01:43:42 +0300

    ReturnRequestTableViewCell

---
+++ PharmacyTask/Modules/ReturnRequest/View/ReturnRequestTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ReturnRequestTableViewCell.swift
+//  PharmacyTask
+//
+//  Created by Mohamed Kotb Saied Kotb on 18/07/2024.
+//
+
+import UIKit
+
+class ReturnRequestTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var createdAtLabel: UILabel!
+    
+    @IBOutlet weak var numOfItemsLabel: UILabel!
+    
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var serviceTypeLabel: UILabel!
+    
+    
+    @IBOutlet weak var assosiatedLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
