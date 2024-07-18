bb69df08649d055f9c8b1c4eb2cec9c74c5234d1
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-07-18 14:30:48 +0300

    Initial Commit

---
+++ PharmacyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PharmacyTask
+//
+//  Created by Mohamed Kotb Saied Kotb on 18/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
